77da4ad40d1643d052bf93a45b7541d53ff3abba
dell <dell@DESKTOP-HFLK4T6>
2019-12-08 13:15:16 +0800

    exp2.over

--- Exp2/2.3.cpp
+++ Exp2/2.3.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
 #include <gl/GLUT.h>
 #include <map>
-#include <vector>
 #include<cmath>
 #include <list>
 #include <algorithm>
 using namespace std;
 
-struct Point {
-	float x, y;
-	Point(float _x, float _y) :x(_x), y(_y) {}
-	Point() {}
-	void operator =(Point& r) {
-		x = r.x; y = r.y;
-	}
-};
-
-Point vertex[2];
-int down_cnt = 1;               //记录点击的次数
 int choice_id = -1;
-int window_size = 800;
-float XL = 200, XR = 600, YB = 200, YT = 600;
+float XL = 100, XR = 400, YB = 100, YT = 600;
 
 void draw_rectangle();
 void grey_line(float x1, float y1, float x2, float y2);
@@ -30,16 +17,14 @@ void OnMouse(int button, int state, int x, int y);
 void InitEnvironment();
 bool ClipT(float p, float q, float* u1, float* u2);
 void LB_LineClip(float x1, float y1, float x2, float y2);
-void myMotion(int x, int y);
 
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);   //初始化GLUT
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 	glutInitWindowPosition(300, 100);
-	glutInitWindowSize(window_size, window_size);
+	glutInitWindowSize(800, 800);
 	glutCreateWindow("liang_Barskey");
 	InitEnvironment();   //初始化
-	glutMotionFunc(&myMotion);   //回调函数
 	glutMouseFunc(&OnMouse);  //注册鼠标事件
 	glutDisplayFunc(draw_rectangle);
 	glutMainLoop();    //持续显示，当窗口改变会重新绘制图形
@@ -54,30 +39,23 @@ void InitEnvironment() {
 	glLoadIdentity();
 	glPointSize(6);
 	glLineWidth(3);
-	gluOrtho2D(0, window_size, 0, window_size);
+	gluOrtho2D(0,800, 0, 800);
 
 	draw_rectangle();
 }
 
-void myMotion(int x, int y) {
-	if (choice_id != -1) {
-		vertex[choice_id].x = float(x);
-		vertex[choice_id].y = float(y);
-		LB_LineClip(vertex[0].x, vertex[0].y, vertex[1].x, vertex[1].y);
-	}
-}
 
 void draw_rectangle() {
 	glBegin(GL_LINES);
 	glColor3f(1, 1, 0);
-	glVertex2f(XL, window_size - YB);
-	glVertex2f(XR, window_size - YB);
-	glVertex2f(XL, window_size - YT);
-	glVertex2f(XR, window_size - YT);
-	glVertex2f(XL, window_size - YB);
-	glVertex2f(XL, window_size - YT);
-	glVertex2f(XR, window_size - YB);
-	glVertex2f(XR, window_size - YT);
+	glVertex2f(XL,  YB);
+	glVertex2f(XR,  YB);
+	glVertex2f(XL,  YT);
+	glVertex2f(XR,  YT);
+	glVertex2f(XL,  YB);
+	glVertex2f(XL,  YT);
+	glVertex2f(XR,  YB);
+	glVertex2f(XR,  YT);
 	glEnd();
 	glFlush();
 }
@@ -85,7 +63,7 @@ void draw_rectangle() {
 void draw_rp(float x, float y) {
 	glBegin(GL_POINTS);
 	glColor3f(1, 0, 0);
-	glVertex2f(x, window_size - y);
+	glVertex2f(x,  y);
 	glEnd();
 	glFlush();
 }
@@ -93,36 +71,20 @@ void OnMouse(int button, int state, int x, int y)
 //OnMouse坐标系x从左向右，y从上到下
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		if (down_cnt == 1) {
-			vertex[0].x = float(x); vertex[0].y = float(y);
-			draw_rp(x, y);
-		}
-		else if (down_cnt == 2) {
-			vertex[1].x = float(x); vertex[1].y = float(y);
-			draw_rp(x, y);
-			LB_LineClip(vertex[0].x, vertex[0].y, vertex[1].x, vertex[1].y);
-		}
-		else {
-			choice_id = -1;
-			for (int i = 0; i < 2; i++) {
-				if (abs(x - vertex[i].x) < 8 && abs(y - vertex[i].y) < 8) {//选中一个点
-					choice_id = i; break;
-				}
-			}
-		}
-		down_cnt++;
+		draw_rectangle();
+		grey_line(50, 50, 500, 800);
 	}
 	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-	{   //点击右键，选择顶点拖动
-
+	{  
+		LB_LineClip(50, 50, 500, 800);
 	}
 }
 
 void grey_line(float x1, float y1, float x2, float y2) {
 	glBegin(GL_LINES);
 	glColor3f(0.5, 0.5, 0.5);
-	glVertex2f(x1, window_size - y1);
-	glVertex2f(x2, window_size - y2);
+	glVertex2f(x1,  y1);
+	glVertex2f(x2,  y2);
 	glEnd();
 	glFlush();
 }
@@ -130,8 +92,8 @@ void grey_line(float x1, float y1, float x2, float y2) {
 void green_line(float x1, float y1, float x2, float y2) {
 	glBegin(GL_LINES);
 	glColor3f(0, 1, 0);
-	glVertex2f(x1, window_size - y1);
-	glVertex2f(x2, window_size - y2);
+	glVertex2f(x1,  y1);
+	glVertex2f(x2,  y2);
 	glEnd();
 	glFlush();
 }
